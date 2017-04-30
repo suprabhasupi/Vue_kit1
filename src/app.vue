52d9b7e99e4703e32d6ddd21e3a915a9f46f5f03
suprabha <suprabha@zoomcar.com>
2017-04-30 18:08:15 +0530

    setup for vue.js

---
+++ src/app.vue
@@ -0,0 +1,23 @@
+<template>
+<p>Hello</p>
+
+
+</template>
+
+<script>
+import './styles/style.scss';
+import './app.scss';
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  mounted: function () {
+
+}
+
+}
+
+</script>
